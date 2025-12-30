ebe43b01fe2faa93b047cd51aeb886973c45047c
Alisienok Nikita <jlucenokgogo@gmail.com>
2025-12-31 02:11:42 +0300

    fix

--- src/main/java/com/urlshortener/controller/AuthController.java
+++ src/main/java/com/urlshortener/controller/AuthController.java
@@ -1,11 +1,6 @@
 package com.urlshortener.controller;
 
-import com.urlshortener.controller.dto.IdResp;
-import com.urlshortener.controller.req.UrlShortCreateReq;
-import com.urlshortener.security.AuthUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +9,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-
-
-
-
-    @PostMapping("/shorten")
-    public IdResp create(@RequestBody UrlShortCreateReq req) {
-        int userId = AuthUtil.getUserId();
-        int urlShortId = urlShortService.create(userId, req);
-        return new IdResp(urlShortId);
-    }
 
 }
